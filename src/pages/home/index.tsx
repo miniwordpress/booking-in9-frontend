d53e1916d131f1fa13c18cfba6e587d92b361b24
Phattarapon (Arm) <OUTSOURCE1887@setth.onmicrosoft.com>
2024-08-27 00:51:49 +0700

    fix: set loading to fix error home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import LocaleSwitcher from "../../components/language_switcher";
 import { useTranslation } from 'react-i18next';
 import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useEffect, useState } from "react";
 
 //TODO: client side session storage
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -13,19 +14,28 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   };
 
-export default function HomePage() {
-  const router = useRouter();
-  const { locale, locales } = router;
-  const { t } = useTranslation('common');
-
-  return (
-    <div>
-      <h1>{t('hello')}</h1>
-      <p>Current locale: {locale}</p>
-      <p>Configured locales: {JSON.stringify(locales)}</p>
-      
-      <LocaleSwitcher />
-
+  export default function HomePage() {
+    const router = useRouter();
+    const { locale, locales } = router;
+    const { t } = useTranslation('common');
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      setLoading(false);
+    }, [locale]);
+  
+    return (
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <h1>{t('hello')}</h1>
+            <p>Current locale: {locale}</p>
+            <p>Configured locales: {JSON.stringify(locales)}</p>
+            <LocaleSwitcher />
+          </>
+        )}
       </div>
-  );
-}
+    );
+  }
