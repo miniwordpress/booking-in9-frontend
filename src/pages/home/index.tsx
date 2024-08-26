7999e2a8e33fcb65bb02b30f0990dc396e4071bd
Phattarapon (Arm) <OUTSOURCE1887@setth.onmicrosoft.com>
2024-08-27 00:44:24 +0700

    fix: error open home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,7 +8,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
       props: {
-        ...(await serverSideTranslations(context.locale || 'en', ['common'])),
+        ...(await serverSideTranslations(context.locale || 'th', ['common'])),
       },
     };
   };
