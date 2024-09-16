17816d0153c735e1a9aae39213bd0b14ecd4ff42
gotnatsarin <gotnatsarin1997@gmail.com>
2024-09-17 01:42:51 +0700

    search hotel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,56 +18,100 @@ export default function SignInPage() {
         <Header />
       </div>
 
-      <div className="flex flex-row bg-[url('/assets/images/friend.jpg')]  w-full">
-        <div className="basis-1/2">
-          <Carousel className="rounded-xl">
+      <div className="flex flex-row  bg-[url('/assets/images/friends.png')] bg-cover bg-center ">
+        <div className="basis-1/2 items-center justify-center">
+        <div className=" ml-8 box-content h-96 w-full p-12">
+
+          <Carousel className="rounded-xl ">
             <img
               src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
               alt="image 1"
               className="h-full w-full object-cover"
-            />
+              />
             <img
               src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
               alt="image 2"
               className="h-full w-full object-cover"
-            />
+              />
             <img
               src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
               alt="image 3"
               className="h-full w-full object-cover"
-            />
+              />
           </Carousel>
+              </div>
         </div>
-        <div className="basis-1/2">
-          <Card className="flex items-center justify-center " color="transparent" shadow={false}>
-            <Typography variant="h4" color="white">
-              {t('forgot_password')}
+        <div className="basis-1/2 flex items-center justify-center ">
+          <Card className="flex " color="transparent" shadow={false}>
+            <Typography variant="h3" color="white" className="[text-shadow:_0_1px_0_rgb(0_0_0_/_80%)]">
+              {t('search_accommodation')}
             </Typography>
-            <Typography className="mt-2" variant="h6" color="white">
-              {t('forgot_password_detail')}
-            </Typography>
-            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 ">
-              <div className="mb-1 flex flex-col gap-6 rounded-lg border-4  border-purple-300 p-6 bg-box-shadow-signin">
-                <Typography variant="h6" color="white" className="-mb-3">
-                  {t('email')}
-                </Typography>
+            <form className="mt-8 mb-2 ">
+              {/* <div className="mb-1 flex flex-col gap-6 rounded-lg border-4  border-purple-300 p-6 bg-box-shadow-signin"> */}
+              <div className="mb-1 flex flex-col gap-6 ">
+                <div className="w-full">
+                <Select label="ตำแหน่งที่ตั้ง" className="bg-white">
+                  <Option>Material Tailwind HTML</Option>
+                  <Option>Material Tailwind React</Option>
+                  <Option>Material Tailwind Vue</Option>
+                  <Option>Material Tailwind Angular</Option>
+                  <Option>Material Tailwind Svelte</Option>
+                </Select>
+                </div>
                 <div className="w-full ">
-                  <Select
-                    className=" !border-t-blue-gray-500 focus:!border-t-gray-900 bg-white"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}>
-                    <Option>Material Tailwind HTML</Option>
-                    <Option>Material Tailwind React</Option>
-                    <Option>Material Tailwind Vue</Option>
-                    <Option>Material Tailwind Angular</Option>
-                    <Option>Material Tailwind Svelte</Option>
-                  </Select>
+                <Select label="จังหวัด/รัฐ" className="bg-white">
+                  <Option>Material Tailwind HTML</Option>
+                  <Option>Material Tailwind React</Option>
+                  <Option>Material Tailwind Vue</Option>
+                  <Option>Material Tailwind Angular</Option>
+                  <Option>Material Tailwind Svelte</Option>
+                </Select>
+                </div>
+                <div className="flex items-center gap-2 ">
+                  <div>
+                    <Select label="ผู้ใหญ่ (13 ปีขึ้นไป)" className="bg-white">
+                      <Option>Material Tailwind HTML</Option>
+                      <Option>Material Tailwind React</Option>
+                      <Option>Material Tailwind Vue</Option>
+                      <Option>Material Tailwind Angular</Option>
+                      <Option>Material Tailwind Svelte</Option>
+                    </Select>
+                  </div>
+                  <div>
+                    <Select label="เด็ก (2-13 ปี)" className="bg-white">
+                      <Option>Material Tailwind HTML</Option>
+                      <Option>Material Tailwind React</Option>
+                      <Option>Material Tailwind Vue</Option>
+                      <Option>Material Tailwind Angular</Option>
+                      <Option>Material Tailwind Svelte</Option>
+                    </Select>
+                  </div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div>
+                    <Select label="ทารก (น้อยกว่า 2 ปี)" className="bg-white">
+                      <Option>Material Tailwind HTML</Option>
+                      <Option>Material Tailwind React</Option>
+                      <Option>Material Tailwind Vue</Option>
+                      <Option>Material Tailwind Angular</Option>
+                      <Option>Material Tailwind Svelte</Option>
+                    </Select>
+                  </div>
+                  <div>
+                    <Select label="สัตว์เลี้ยง" className="bg-white">
+                      <Option>Material Tailwind HTML</Option>
+                      <Option>Material Tailwind React</Option>
+                      <Option>Material Tailwind Vue</Option>
+                      <Option>Material Tailwind Angular</Option>
+                      <Option>Material Tailwind Svelte</Option>
+                    </Select>
+                  </div>
                 </div>
                 <Button className="mt-2 bg-gradient-to-r from-cyan-500 via-purple-300 to-pink-300" fullWidth>
-                  {t('signIn')}
+                  {t('search')}
                 </Button>
               </div>
+              
             </form>
           </Card>
         </div>
