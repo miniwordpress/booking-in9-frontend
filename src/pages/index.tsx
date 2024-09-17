20c37c573fa9a02e99f6fe7177aefb7b389f4258
gotnatsarin <gotnatsarin1997@gmail.com>
2024-09-18 01:45:03 +0700

    card main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { useTranslation } from "react-i18next"
-import Header from "../components/header"
-import Footer from "../components/footer"
+import { useTranslation } from "react-i18next";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import {
   Carousel,
   Card,
@@ -8,10 +8,14 @@ import {
   Option,
   Typography,
   Button,
-} from "@material-tailwind/react"
+  Avatar,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from "@material-tailwind/react";
 
 export default function SignInPage() {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   return (
     <>
       <div className=" bg-fixed">
@@ -19,57 +23,60 @@ export default function SignInPage() {
       </div>
 
       <div className="flex flex-row  bg-[url('/assets/images/friends.png')] bg-cover bg-center ">
-        <div className="basis-1/2 items-center justify-center">
-        <div className=" ml-8 box-content h-96 w-full p-12">
-
-          <Carousel className="rounded-xl ">
-            <img
-              src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-              alt="image 1"
-              className="h-full w-full object-cover"
+        <div className="basis-1/2 items-center justify-center rounded-md h-full w-ful">
+          <div className=" ml-8 box-content  p-16">
+            <Carousel className="rounded-xl ">
+              <img
+                src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                alt="image 1"
+                className="h-full w-full object-cover"
               />
-            <img
-              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-              alt="image 2"
-              className="h-full w-full object-cover"
+              <img
+                src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                alt="image 2"
+                className="h-full w-full object-cover"
               />
-            <img
-              src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-              alt="image 3"
-              className="h-full w-full object-cover"
+              <img
+                src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                alt="image 3"
+                className="h-full w-full object-cover"
               />
-          </Carousel>
-              </div>
+            </Carousel>
+          </div>
         </div>
         <div className="basis-1/2 flex items-center justify-center ">
           <Card className="flex " color="transparent" shadow={false}>
-            <Typography variant="h3" color="white" className="[text-shadow:_0_1px_0_rgb(0_0_0_/_80%)]">
-              {t('search_accommodation')}
+            <Typography
+              variant="h3"
+              color="white"
+              className="[text-shadow:_0_1px_0_rgb(0_0_0_/_80%)]"
+            >
+              {t("search_accommodation")}
             </Typography>
             <form className="mt-8 mb-2 ">
               {/* <div className="mb-1 flex flex-col gap-6 rounded-lg border-4  border-purple-300 p-6 bg-box-shadow-signin"> */}
-              <div className="mb-1 flex flex-col gap-6 ">
-                <div className="w-full">
-                <Select label="ตำแหน่งที่ตั้ง" className="bg-white">
-                  <Option>Material Tailwind HTML</Option>
-                  <Option>Material Tailwind React</Option>
-                  <Option>Material Tailwind Vue</Option>
-                  <Option>Material Tailwind Angular</Option>
-                  <Option>Material Tailwind Svelte</Option>
-                </Select>
-                </div>
+              <div className="mb-1 flex flex-col gap-8 ">
                 <div className="w-full ">
-                <Select label="จังหวัด/รัฐ" className="bg-white">
-                  <Option>Material Tailwind HTML</Option>
-                  <Option>Material Tailwind React</Option>
-                  <Option>Material Tailwind Vue</Option>
-                  <Option>Material Tailwind Angular</Option>
-                  <Option>Material Tailwind Svelte</Option>
-                </Select>
+                  <Select label={t("Location")} className="bg-white h-14">
+                    <Option>Material Tailwind HTML</Option>
+                    <Option>Material Tailwind React</Option>
+                    <Option>Material Tailwind Vue</Option>
+                    <Option>Material Tailwind Angular</Option>
+                    <Option>Material Tailwind Svelte</Option>
+                  </Select>
+                </div>
+                <div className="w-full">
+                  <Select label={t("Province_State")} className="bg-white h-14">
+                    <Option>Material Tailwind HTML</Option>
+                    <Option>Material Tailwind React</Option>
+                    <Option>Material Tailwind Vue</Option>
+                    <Option>Material Tailwind Angular</Option>
+                    <Option>Material Tailwind Svelte</Option>
+                  </Select>
                 </div>
                 <div className="flex items-center gap-2 ">
                   <div>
-                    <Select label="ผู้ใหญ่ (13 ปีขึ้นไป)" className="bg-white">
+                    <Select label={t("adult")} className="bg-white h-14">
                       <Option>Material Tailwind HTML</Option>
                       <Option>Material Tailwind React</Option>
                       <Option>Material Tailwind Vue</Option>
@@ -78,7 +85,7 @@ export default function SignInPage() {
                     </Select>
                   </div>
                   <div>
-                    <Select label="เด็ก (2-13 ปี)" className="bg-white">
+                    <Select label={t("Children")} className="bg-white h-14">
                       <Option>Material Tailwind HTML</Option>
                       <Option>Material Tailwind React</Option>
                       <Option>Material Tailwind Vue</Option>
@@ -89,7 +96,7 @@ export default function SignInPage() {
                 </div>
                 <div className="flex items-center gap-2">
                   <div>
-                    <Select label="ทารก (น้อยกว่า 2 ปี)" className="bg-white">
+                    <Select label={t("infant")} className="bg-white h-14">
                       <Option>Material Tailwind HTML</Option>
                       <Option>Material Tailwind React</Option>
                       <Option>Material Tailwind Vue</Option>
@@ -98,7 +105,7 @@ export default function SignInPage() {
                     </Select>
                   </div>
                   <div>
-                    <Select label="สัตว์เลี้ยง" className="bg-white">
+                    <Select label={t("pets")} className="bg-white h-14">
                       <Option>Material Tailwind HTML</Option>
                       <Option>Material Tailwind React</Option>
                       <Option>Material Tailwind Vue</Option>
@@ -107,16 +114,102 @@ export default function SignInPage() {
                     </Select>
                   </div>
                 </div>
-                <Button className="mt-2 bg-gradient-to-r from-cyan-500 via-purple-300 to-pink-300" fullWidth>
-                  {t('search')}
+                <Button
+                  className="mt-2 bg-gradient-to-r from-cyan-500 via-purple-300 to-pink-300"
+                  fullWidth
+                >
+                  <Typography variant="h5">{t("search")}</Typography>
                 </Button>
               </div>
-              
             </form>
           </Card>
         </div>
       </div>
+      <>
+        <div className="flex items-center justify-center mt-8 mb-8">
+          <Card className="rounded-lg bg-zinc-300">
+            <div className="p-6 w-auto  grid grid-cols-5 gap-8 ">
+              <div className="">
+                <Avatar
+                  size="xxl"
+                  alt="avatar"
+                  src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  className="border border-green-500 shadow-xl hover:ring ring-pink-200 shadow-pink-900/20 "
+                />
+                <Typography className="text-center mt-4" variant="h6">
+                  {t("บ้านเดียว")}
+                </Typography>
+              </div>
+              <div className="">
+                <Avatar
+                  size="xxl"
+                  alt="avatar"
+                  src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  className="border border-green-500 shadow-xl hover:ring ring-pink-200 shadow-pink-900/20 "
+                />
+                <Typography className="text-center mt-4" variant="h6">
+                  {t("โรงแรม")}
+                </Typography>
+              </div>
+              <div className="">
+                <Avatar
+                  size="xxl"
+                  alt="avatar"
+                  src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  className="border border-green-500 shadow-xl hover:ring ring-pink-200 shadow-pink-900/20 "
+                />
+                <Typography className="text-center mt-4" variant="h6">
+                  {t("อพาร์ทเมนท์")}
+                </Typography>
+              </div>
+              <div className="">
+                <Avatar
+                  size="xxl"
+                  alt="avatar"
+                  src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  className="border border-green-500 shadow-xl hover:ring ring-pink-200 shadow-pink-900/20 "
+                />
+                <Typography className="text-center mt-4" variant="h6">
+                  {t("บ้านทาวน์โฮม")}
+                </Typography>
+              </div>
+              <div className="">
+                <Avatar
+                  size="xxl"
+                  alt="avatar"
+                  src="https://docs.material-tailwind.com/img/face-2.jpg"
+                  className="border border-green-500 shadow-xl hover:ring ring-pink-200 shadow-pink-900/20 "
+                />
+                <Typography className="text-center mt-4" variant="h6">
+                  {t("คอนโด")}
+                </Typography>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </>
+      <>
+        <div className="flex items-center justify-center mt-8 mb-8">
+          <div className="p-8 w-full  grid grid-cols-5 gap-4 ">
+          <Card className="w-92 shadow-xl hover:shadow-orange-900/50">
+      <CardHeader floated={false} className="h-80">
+        <img src="https://docs.material-tailwind.com/img/team-3.jpg" alt="profile-picture" />
+      </CardHeader>
+      <CardBody className="text-center">
+        <Typography variant="h4" color="blue-gray" className="mb-2">
+          Natalie Paisley
+        </Typography>
+        <Typography color="blue-gray" className="font-medium" textGradient>
+          CEO / Co-Founder
+        </Typography>
+      </CardBody>
+            </Card>
+            
+           
+          </div>
+        </div>
+      </>
       <Footer />
     </>
-  )
+  );
 }
