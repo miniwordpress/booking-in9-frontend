4f3068326f6105a3fafa8e21df25fcab55d3774a
unknown <gotnatsarin1997@gmail.com>
2024-09-22 21:51:03 +0700

    menu

---
+++ src/components/header_admin.tsx
@@ -0,0 +1,92 @@
+import React from "react"
+import { useRouter } from "next/router"
+import Image from "next/image"
+import LocaleSwitcher from "../components/language_switcher"
+import { useEffect, useState } from "react"
+import {
+  Navbar,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react"
+
+const HeaderBaseAdmin: React.FC = () => {
+  const router = useRouter()
+  const { locale } = router
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(false)
+  }, [locale])
+
+  const [openNav, setOpenNav] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    )
+  }, [])
+
+  return (
+    <div className="max-h-[768px] w-[calc(100%)] bg-white">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4  bg-primary-background" style={{ border: "none" }}>
+        <div className="flex justify-end text-blue-gray-900">
+          <div className="flex gap-4">
+            {loading ? (<></>) : (<LocaleSwitcher />)}
+            <Typography>
+              <a href="">
+                <Image
+                  src="/assets/images/help.png"
+                  alt="facebook Logo"
+                  color="white"
+                  width={30}
+                  height={30}
+                  priority
+                />
+              </a>
+            </Typography>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+      </Navbar>
+    </div>
+  )
+}
+
+export default HeaderBaseAdmin
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,3 +1,5 @@
+import SidebarMenu from "@/components/SidebarMenu";
+import HeaderBaseAdmin from "../../components/header_admin";
 import {
   Card,
   CardHeader,
@@ -5,39 +7,171 @@ import {
   CardFooter,
   Typography,
   Button,
+  Input,
+  Chip,
+  Tabs,
+  TabsHeader,
+  Tab,
+  Avatar,
+  IconButton,
+  Tooltip,
 } from "@material-tailwind/react";
+
+const TABLE_HEAD = ["ลำดับ", "ชื่อที่พัก", "สถานะ", "จัดการ"];
+ 
+const TABLE_ROWS = [
+  {
+    name: "1",
+    job: "Manager",
+    date: "23/04/18",
+  },
+  {
+    name: "2",
+    job: "Developer",
+    date: "23/04/18",
+  },
+  {
+    name: "3",
+    job: "Executive",
+    date: "19/09/17",
+  },
+  {
+    name: "4",
+    job: "Developer",
+    date: "24/12/08",
+  },
+  {
+    name: "5",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "6",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "7",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "8",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "9",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "10",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "11",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "12",
+    job: "Manager",
+    date: "04/10/21",
+  },
+  {
+    name: "13",
+    job: "Manager",
+    date: "04/10/21",
+  },
+];
 
 export function Card5() {
   return (
-    <section className="grid h-screen place-items-center p-4 ">
-      <Card className="w-auto max-w-[24rem]">
-        <CardHeader floated={false} shadow={false} className="rounded-none">
-          <Typography
-            variant="small"
-            color="blue-gray"
-            className="font-medium"
-          >
-            Enterprise
-          </Typography>
-          <Typography
-            color="blue-gray"
-            className="mt-1 mb-2 text-[20px] font-bold"
-          >
-            Autodesk looks to future of 3D printing with Project Escher
-          </Typography>
-        </CardHeader>
-        <CardBody className="px-4 pt-0">
-          <Typography className="font-normal text-gray-600">
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers. I understand culture. I am
-            the nucleus.
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0 px-4">
-          <Button>read more</Button>
-        </CardFooter>
-      </Card>
-    </section>
+    <div className="flex flex-row h-full w-full">
+      <div className="basis-1/5 h-full">
+        <SidebarMenu/>
+      </div>
+      <div className="basis-1/1 w-full">
+        <HeaderBaseAdmin/>
+        <div className="bg-white p-12 ">
+        <Card className="w-full ">
+      <table className="w-full min-w-max table-auto text-left">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head) => (
+              <th
+                key={head}
+                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+              >
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {TABLE_ROWS.map(({ name, job, date }, index) => {
+            const isLast = index === TABLE_ROWS.length - 1;
+            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+ 
+            return (
+              <tr key={name}>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {name}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {job}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {date}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    as="a"
+                    href="#"
+                    variant="small"
+                    color="blue-gray"
+                    className="font-medium"
+                  >
+                    Edit
+                  </Typography>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Card>
+        </div>
+      </div>
+      </div>
+
+      
   );
 }
 
