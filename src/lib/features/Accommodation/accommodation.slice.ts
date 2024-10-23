9c4b6c5177f1693ebdfa5c54f203c540fd4759cb
MuangaekP <muangaek.u@gmail.com>
2024-10-24 00:29:36 +0700

    addcoomodation reducer v.2

--- src/lib/features/Accommodation/accommodation.slice.ts
+++ src/lib/features/Accommodation/accommodation.slice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { listAccommodation } from "./accommodation.query"
-import { setAccessToken } from '@/utils/cookies'
 import { IReduxAccommodation } from "./accommodation"
 
 const reducerName = "accommodation"
